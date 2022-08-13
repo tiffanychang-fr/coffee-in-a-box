66c8b2bcd3eaa34644c9ce806a681ff00d57b6b9
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-08-13 17:26:26 +0200

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("coffee-in-a-box JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/coffee-in-a-box";
+
+module.exports = MONGO_URI;
