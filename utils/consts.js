0244dc095daa2acfb7c2e72d2a0f445339956360
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-08-14 10:27:38 +0200

    add mongoDB atlas connection

--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,4 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/coffee-in-a-box";
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost/coffee-in-a-box";
 
 module.exports = MONGO_URI;
